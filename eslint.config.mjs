d2cc73ed5132212076e2d5da0f678ffb98e4d4f7
Luis Grizzo <luisoggrizzo@gmail.com>
2025-12-30 13:33:41 -0300

    chore: add configuration files for ESLint, Prettier, and EditorConfig; update styles and imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      'prettier/prettier': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+      'no-console': ['warn', { allow: ['error'] }]
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    '/prisma'
+  ])
+])
 
-export default eslintConfig;
+export default eslintConfig
